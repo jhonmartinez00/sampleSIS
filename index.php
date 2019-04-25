efe9c59421046741c21410ed5cffbcc4ca779d81
jhon martinez <jhonmartinez.exec@gmail.com>
2019-04-25 16:54:10 +0800

    wew

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<title>Student Information System</title>
+</head>
+<body>
+<div class="container">
+	<h1 class="title">Student Information System</h1>
+	<div class="container1">
+	<a href="create.php"><button class="btn btn-success">Create</button></a>
+	<a href="read.php"><button class="btn btn-primary">Read</button></a>
+	<a href="update.php"><button class="btn btn-secondary">Update</button></a>
+	<a href="delete.php"><button class="btn btn-danger">Delete</button></a>
+	</div>
+</div>
+</body>
+</html>
